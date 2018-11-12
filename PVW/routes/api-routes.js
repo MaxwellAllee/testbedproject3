ebad881409f4a581917815cc51f7fc3b354c0f6a
killercoder <maxwell.w.allee@gmail.com>
2018-11-12 17:12:50 -0500

    another push to tower

--- PVW/routes/api-routes.js
+++ PVW/routes/api-routes.js
@@ -10,75 +10,72 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Mail.findAll({})
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
+module.exports = function (app) {
   // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function(req, res) {
+  app.post("/api/mail/receiver", function (req, res) {
     db.Mail.findAll({
       where: {
-        category: req.params.category
+        receiver: req.body.receiver,
+        recieverDelete: false
       }
     })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
 
-  // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    db.Mail.findOne({
-      where: {
-        id: req.params.id
-      }
+      .then(function (dbMessage) {
+        console.log(dbMessage)
+        res.json(dbMessage)
+      })
     })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Mail.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Mail.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Mail.update(req.body,
-      {
+    // Get route for retrieving a single post
+    app.get("/api/posts/:id", function (req, res) {
+      db.Mail.findOne({
         where: {
-          id: req.body.id
+          id: req.params.id
         }
       })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-};
+        .then(function (dbPost) {
+          res.json(dbPost);
+        });
+    });
+
+    // POST route for saving a new messages
+    app.post("/api/mail/send", function (req, res) {
+      console.log(req.body);
+      db.Mail.create({
+        sender: req.body.sender,
+        receiver: req.body.receiver,
+        title: req.body.title,
+        body: req.body.body
+      })
+        .then(function (dbPost) {
+          res.json(dbPost);
+        });
+    })
+
+
+    // DELETE route for deleting posts
+    app.delete("/api/posts/:id", function (req, res) {
+      db.Mail.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(function (dbPost) {
+          res.json(dbPost);
+        });
+    });
+
+    // PUT route for updating posts
+    app.put("/api/posts", function (req, res) {
+      db.Mail.update(req.body,
+        {
+          where: {
+            id: req.body.id
+          }
+        })
+        .then(function (dbPost) {
+          res.json(dbPost);
+        });
+    });
+  };
